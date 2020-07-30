adc1b77f9dadd3d45b6e78164e3a4e2c3bda5730
Jannik <jw19980309@gmail.com>
2020-07-30 14:21:18 +0200

    Updated Responsiveness

--- components/sourcesContainer.tsx
+++ components/sourcesContainer.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 export default class SourcesContainer extends React.Component {
-  render() {
+  render(): JSX.Element {
     return (
       <div
         style={{
           display: "flex",
           alignContent: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          gap: "20px",
         }}
       >
         {this.props.children}
