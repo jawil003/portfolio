248f873b0d3703fb6e1b3c182f52ed694d063a5f
Jannik <jw19980309@gmail.com>
2020-12-02 00:04:41 +0100

    Readded Title on Index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,7 @@ import Spacer from "../components/elements/Spacer";
 import LinkedInIcon from "../components/icons/linkedin.icon";
 import GithubIcon from "../components/icons/github.icon";
 import IconLink from "../components/elements/IconLink";
-import { motion, useSpring, useTransform } from "framer-motion";
-import flyFromTop from "../variants/flyFromTop";
+import { useSpring, useTransform } from "framer-motion";
 import DribbleIcon from "../components/icons/dribble.icon";
 import HeaderWithImage from "../components/HeaderWithImage";
 
@@ -26,57 +25,62 @@ const Index: React.FC<Props> = () => {
     handleImageLoaded();
   }, []);
   return (
-    <HeaderWithImage
-      latest
-      basePath="img/dortmund-u_jkjc9j/"
-      image={"dortmund-u_jkjc9j_c_scale,w_1400.jpg"}
-      imageSet={[
-        "dortmund-u_r6ij9f_c_scale,w_200.jpg 200w.jpg",
-        "dortmund-u_r6ij9f_c_scale,w_484.jpg 484w.jpg",
-        "dortmund-u_r6ij9f_c_scale,w_683.jpg 683w.jpg",
-        "dortmund-u_r6ij9f_c_scale,w_855.jpg 855w.jpg",
-        "dortmund-u_r6ij9f_c_scale,w_997.jpg 997w.jpg",
-        "dortmund-u_r6ij9f_c_scale,w_1121.jpg 1121w.jpg",
-        "dortmund-u_r6ij9f_c_scale,w_1259.jpg 1259w.jpg",
-        "dortmund-u_r6ij9f_c_scale,w_1383.jpg 1383w.jpg",
-        "dortmund-u_r6ij9f_c_scale,w_1400.jpg 1400w.jpg",
-      ]}
-      sizes="(max-width: 1400px) 100vw, 1400px"
-      alt="Dortmunder U"
-    >
-      <Typography variant="h3" align="center">
-        Hallo, ich bin Jannik
-      </Typography>
-      <Spacer height="0.4em" />
-      <Typography align="center" italic>
-        ein leidenschaftlicher Softwareentwickler und Designer aus Dortmund und
-        spezialisiert auf Frontend und Backend Lösungen aller Art
-      </Typography>
-      <Spacer height="35px" />
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-around",
-          alignItems: "center",
-        }}
+    <>
+      <Head>
+        <title>Jannik Will | Startseite</title>
+      </Head>
+      <HeaderWithImage
+        latest
+        basePath="img/dortmund-u_jkjc9j/"
+        image={"dortmund-u_jkjc9j_c_scale,w_1400.jpg"}
+        imageSet={[
+          "dortmund-u_r6ij9f_c_scale,w_200.jpg 200w.jpg",
+          "dortmund-u_r6ij9f_c_scale,w_484.jpg 484w.jpg",
+          "dortmund-u_r6ij9f_c_scale,w_683.jpg 683w.jpg",
+          "dortmund-u_r6ij9f_c_scale,w_855.jpg 855w.jpg",
+          "dortmund-u_r6ij9f_c_scale,w_997.jpg 997w.jpg",
+          "dortmund-u_r6ij9f_c_scale,w_1121.jpg 1121w.jpg",
+          "dortmund-u_r6ij9f_c_scale,w_1259.jpg 1259w.jpg",
+          "dortmund-u_r6ij9f_c_scale,w_1383.jpg 1383w.jpg",
+          "dortmund-u_r6ij9f_c_scale,w_1400.jpg 1400w.jpg",
+        ]}
+        sizes="(max-width: 1400px) 100vw, 1400px"
+        alt="Dortmunder U"
       >
-        <IconLink external href="https://twitter.com/willey3x37">
-          <TwitterIcon height="100%" />
-        </IconLink>
-        <IconLink external href="https://linkedin.com/willey3x37">
-          <LinkedInIcon height="100%" />
-        </IconLink>
+        <Typography variant="h3" align="center">
+          Hallo, ich bin Jannik
+        </Typography>
+        <Spacer height="0.4em" />
+        <Typography align="center" italic>
+          ein leidenschaftlicher Softwareentwickler und Designer aus Dortmund
+          und spezialisiert auf Frontend und Backend Lösungen aller Art
+        </Typography>
+        <Spacer height="35px" />
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <IconLink external href="https://twitter.com/willey3x37">
+            <TwitterIcon height="100%" />
+          </IconLink>
+          <IconLink external href="https://linkedin.com/willey3x37">
+            <LinkedInIcon height="100%" />
+          </IconLink>
 
-        <IconLink external href="https://github.com/jawil003">
-          <GithubIcon height="100%" />
-        </IconLink>
+          <IconLink external href="https://github.com/jawil003">
+            <GithubIcon height="100%" />
+          </IconLink>
 
-        <IconLink external href="https://dribbble.com/willey3x37">
-          <DribbleIcon height="100%" />
-        </IconLink>
-      </div>
-    </HeaderWithImage>
+          <IconLink external href="https://dribbble.com/willey3x37">
+            <DribbleIcon height="100%" />
+          </IconLink>
+        </div>
+      </HeaderWithImage>
+    </>
   );
 };
 
