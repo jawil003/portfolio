497fde3126cb743a4c938486b72612c82b1f6ce5
Jannik <jw19980309@gmail.com>
2021-04-24 09:19:50 +0200

    Update Tests

--- components/elements/shared/Typography/Typography.test.tsx
+++ components/elements/shared/Typography/Typography.test.tsx
@@ -10,4 +10,44 @@ describe("Typography", () => {
       testH1Typography.baseElement,
     ).toMatchSnapshot();
   });
+  test("Create H2 Element", () => {
+    const testH2Typography = render(
+      <Typography variant="h2" />,
+    );
+    expect(
+      testH2Typography.baseElement,
+    ).toMatchSnapshot();
+  });
+  test("Create H3 Element", () => {
+    const testH3Typography = render(
+      <Typography variant="h3" />,
+    );
+    expect(
+      testH3Typography.baseElement,
+    ).toMatchSnapshot();
+  });
+  test("Create H4 Element", () => {
+    const testH4Typography = render(
+      <Typography variant="h4" />,
+    );
+    expect(
+      testH4Typography.baseElement,
+    ).toMatchSnapshot();
+  });
+  test("Create H5 Element", () => {
+    const testH5Typography = render(
+      <Typography variant="h5" />,
+    );
+    expect(
+      testH5Typography.baseElement,
+    ).toMatchSnapshot();
+  });
+  test("Create H6 Element", () => {
+    const testH6Typography = render(
+      <Typography variant="h6" />,
+    );
+    expect(
+      testH6Typography.baseElement,
+    ).toMatchSnapshot();
+  });
 });
