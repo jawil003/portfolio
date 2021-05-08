9412245c512576b0f012074674d70465aefe9446
Jannik <jw19980309@gmail.com>
2021-05-08 12:38:27 +0200

    Updated Cypress Test

--- cypress/integration/index.test.ts
+++ cypress/integration/index.test.ts
@@ -19,5 +19,26 @@ describe("ContactForm", () => {
     cy.get(
       ".css-xbw9dc-FlexContainer > #heading4",
     ).scrollIntoView();
+
+    cy.get(
+      '[data-testid="index-contact-form-email-input"]',
+    ).type("maxine@musterfrau.de");
+
+    cy.get(
+      '[data-testid="index-contact-form-name-input"]',
+    ).type("Maxine Musterfrau");
+
+    cy.get(
+      '[data-testid="index-contact-form-title-input"]',
+    ).type("Anfrage");
+
+    cy.get(
+      '[data-testid="index-contact-form-message-textarea"]',
+    ).type(
+      "Sehr geehrter Herr Will,\nich möchte ihnen auf diesem Wege eine nette Testnachricht schreiben.\nWas mich dazu bewegt hat, keine Ahnung.\nAber vielleicht freuen Sie sich ja trotzdem.",
+    );
+    cy.get(
+      "[data-testid=index-contact-form-submit-button]",
+    ).click();
   });
 });
